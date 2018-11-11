612f228ac139a8edb6ffcad04c135db837bf0bb7
Ivars Rozhleys <noname@noname.com>
2018-11-11 22:18:50 +0300

    Игра автоматически закрывается если шарик попадает на южную стену.

---
+++ Assets/Scripts/SouthWallBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SouthWallBehaviour: MonoBehaviour {
+
+    private void OnCollisionEnter2D (Collision2D other) {
+        BallBehaviour ball = other.gameObject.GetComponent<BallBehaviour>();
+        if (ball != null) {
+            // К нам попал шарик. Всё очень плохо. Игрок ни на что не способен.
+            // Дальше нам делать нечего. Убиваемся от позора и стыда...
+            Debug.Log("Are you absolutely positively sure you know how to play this game?");
+            Application.Quit();
+        }
+    }
+
+}
