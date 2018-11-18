765895ca1d696176836a48c72f73eced5cb315c5
Ivars Rozhleys <noname@noname.com>
2018-11-18 18:38:04 +0300

    Добавлена возможность менять высотукамеры. Теперь управление более динамичное чем раньше.

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraBehaviour : MonoBehaviour
-{
+public class CameraBehaviour: MonoBehaviour {
 
-    private void rotateAzimuth(System.Single input)
-    {
+    private void rotateAzimuth(System.Single input) {
         // Получаем текущее расположение камеры относительно точки в пространстве.
         Vector3 pos = transform.position - _pinPoint;
 
@@ -18,7 +16,7 @@ public class CameraBehaviour : MonoBehaviour
         System.Single radius = posXZ.magnitude;
 
         System.Single phi = Vector2.SignedAngle(Vector2.right, posXZ) * Mathf.Deg2Rad;
-        System.Single phiToAdd = input * _speed * Time.deltaTime;
+        System.Single phiToAdd = input * _azimuthSpeed * Time.deltaTime;
 
         phi = phi + phiToAdd;
 
@@ -29,32 +27,39 @@ public class CameraBehaviour : MonoBehaviour
         transform.position = pos + _pinPoint;
     }
 
-    private void rotateElevation(System.Single input)
-    {
+    private void changeHeight(System.Single input) {
+        Vector3 pos = transform.position;
 
+        pos.y += input * _heightSpeed * Time.deltaTime;
+
+        if(pos.y > _heightMax)
+            pos.y = _heightMax;
+
+        if(pos.y < _heightMin)
+            pos.y = _heightMin;
+
+        transform.position = pos;
     }
 
-    private void lookAtPoint()
-    {
+    private void turnToPoint() {
         Vector3 dir = _pinPoint - transform.position;
         dir.Normalize();
         transform.rotation = Quaternion.LookRotation(dir);
     }
 	
-	void LateUpdate()
-    {
+	void LateUpdate() {
         // Здесь логика такая: если нам разрешено вращаться, то вращаемся, если
         // нет то на нет и суда нет. Если вращаемся то сперва поправляем
         // азимутальный угол, потом угол элевации а уже в самом конце поправляем
         // нормаль камеры чтобы мы всегда смотрели на заданую нами точку.
-        if(_allowRotation)
-        {
+        if(_allowRotation) {
             System.Single mouseInputX = Input.GetAxis("Mouse X");
             System.Single mouseInputY = Input.GetAxis("Mouse Y");
 
             rotateAzimuth(mouseInputX);
-            rotateElevation(mouseInputY);
-            lookAtPoint();
+            changeHeight(mouseInputY);
+
+            turnToPoint();
         }
     }
 
@@ -64,9 +69,15 @@ public class CameraBehaviour : MonoBehaviour
     public System.Boolean _allowRotation = false;
 
     // Скорость вращения камеры.
-    public System.Single _speed = 1.0F;
+    public System.Single _azimuthSpeed = 1.0F;
+    public System.Single _heightSpeed = 1.0F;
 
     // Точка на котору направлена камера.
     public Vector3 _pinPoint = new Vector3(0.0F, 0.0F, 0.0F);
+
+    // На какую величину камера может менять свою высоту относительно
+    // начального значения.
+    public System.Single _heightMin = 0.0F;
+    public System.Single _heightMax = 1.0F;
 
 }
