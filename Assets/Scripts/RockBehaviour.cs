ced18877cd8f15da8ebab89cd7b47043b076e2a5
Ivars Rozhleys <noname@noname.com>
2018-11-26 00:29:30 +0300

    Добавлены префабы камней. Добавлен скрипт который генерирует камни.

---
+++ Assets/Scripts/RockBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBehaviour: MonoBehaviour {
+
+    void Awake() {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate () {
+        Vector3 position = _rigidbody.position;
+        position.z -= _speed * Time.deltaTime;
+        _rigidbody.MovePosition(position);
+
+        // Если булыжник улетел слишком далеко за камеру то он самоуничтожается.
+        if(position.z < _deadEnd)
+            Object.Destroy(gameObject);
+	}
+
+    public System.Single _speed = 1.0F;
+
+    // Устанавливает границу по оси Y после которой камень умирает.
+    public System.Single _deadEnd = -10.0F;
+
+    public Rigidbody _rigidbody;
+
+}
--- Assets/Scripts/UFOShakeBehaviour.cs
+++ Assets/Scripts/UFOShakeBehaviour.cs
@@ -4,10 +4,65 @@ using UnityEngine;
 
 public class UFOShakeBehaviour : MonoBehaviour {
 
-	void LateUpdate() {
+    void Awake() {
+        Vector3 startingAngles = new Vector3(
+            Random.Range(-10.0F, +10.0F) * _amplitudeFactor,
+            Random.Range(-10.0F, +10.0F) * _amplitudeFactor,
+            Random.Range(-10.0F, +10.0F) * _amplitudeFactor);
 
+        _times = new Vector3(
+            Random.Range(0.0F, +10.0F) * _timeFactor,
+            Random.Range(0.0F, +10.0F) * _timeFactor,
+            Random.Range(0.0F, +10.0F) * _timeFactor);
+
+        transform.Rotate(startingAngles);
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+	void LateUpdate() {
+        Rotate();
+        Levitate();
 	}
 
-    GameObject _object;
+    void Levitate() {
+        System.Single pingPong = Mathf.PingPong(Time.time, _levitationTime) / _levitationTime;
+        Debug.Log(pingPong);
+        System.Single amplitude = Mathf.Lerp(-_levitationAmplitude, +_levitationAmplitude, pingPong);
+        Vector3 position = _rigidbody.position;
+        position.y += amplitude;
+        _rigidbody.MovePosition(position);
+    }
+
+    void Rotate() {
+        Vector3 pingPongs = new Vector3(
+            Mathf.PingPong(Time.time, _times.x) / _times.x,
+            Mathf.PingPong(Time.time, _times.y) / _times.y,
+            Mathf.PingPong(Time.time, _times.z) / _times.z);
+
+        Vector3 rotations = new Vector3(
+            Mathf.Lerp(-10.0F, +10.0F, pingPongs.x) * _amplitudeFactor,
+            Mathf.Lerp(-10.0F, +10.0F, pingPongs.y) * _amplitudeFactor,
+            0.0F/*Mathf.Lerp(-10.0F, +10.0F, pingPongs.z)*/);
+
+        //Vector3 rotation = _object.transform.rotation;
+        //rotation.x += 
+
+        //_object.transform.Rotate(rotations);
+    }
+
+    // Объект над которым производим вращение.
+    public GameObject _object = null;
+
+    public System.Single _levitationAmplitude = 0.01F;
+    public System.Single _levitationTime = 1.0F;
+
+    // Сила с которой колеблется объект.
+    public System.Single _amplitudeFactor = 1.0F;
+    public System.Single _timeFactor = 1.0F;
+
+    private Rigidbody _rigidbody;
+
+    //private Vector3 _startingAngles;
+    private Vector3 _times;
 
 }
