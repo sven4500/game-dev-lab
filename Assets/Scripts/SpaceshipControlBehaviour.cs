47f997f4284ee3afa9a8454e1c20dc6412208472
Ivars Rozhleys <noname@noname.com>
2018-12-02 18:46:48 +0300

    Добавлены текстуры для кратера.

--- Assets/Scripts/SpaceshipControlBehaviour.cs
+++ Assets/Scripts/SpaceshipControlBehaviour.cs
@@ -12,14 +12,16 @@ public class SpaceshipControlBehaviour : MonoBehaviour {
         System.Single inpX = -1.0F * Input.GetAxis("Horizontal");
         System.Single inpZ = -1.0F * Input.GetAxis("Vertical");
         
-        System.Single acc = _acceleration;
-
-        if(inpX == 0.0F && inpZ == 0)
-            acc = -1.0F * _slowing;
+        Vector3 euler = _rb.rotation.eulerAngles;
 
         if(inpX != 0.0F) {
-            _dir.x += Mathf.Sign(inpX) * _reaction * Time.deltaTime;
-            _dir.x = Mathf.Clamp(_dir.x, -1.0F, 1.0F);
+            euler.z += Mathf.Sign(inpX) * Mathf.Sign(inpZ) * _angularReaction * Time.deltaTime;
+            if(inpZ != 0.0F) {
+                _dir.x += Mathf.Sign(inpX) * _reaction * Time.deltaTime;
+                _dir.x = Mathf.Clamp(_dir.x, -1.0F, 1.0F);
+            } else {
+                euler.z *= -1.0F;
+            }
         } else if(Mathf.Abs(_dir.x) > 0.0F) {
             System.Single val = Mathf.Abs(_dir.x) - _reaction * Time.deltaTime;
             val = Mathf.Clamp(val, 0.0F, 1.0F);
@@ -35,15 +37,15 @@ public class SpaceshipControlBehaviour : MonoBehaviour {
             _dir.z = Mathf.Sign(_dir.z) * val;
         }
 
-        //_dir.Normalize();
-        Debug.Log(_dir);
-        
+        // Здесь в зависимости от наличия управления задаём тормозящее ускорение.
+        // В случае если кнопки нажаты, то используется обычное ускорение.
+        System.Single acc = (inpX != 0.0F || inpZ != 0) ? _acceleration : -1.0F * _slowing;
         _speed += acc * Time.deltaTime * Time.deltaTime;
         _speed = Mathf.Clamp(_speed, 0.0F, _maxSpeed);
-        //Debug.Log(_speed);
 
         if(_speed != 0.0F) {
-            _rb.MovePosition(_rb.position + _dir * _speed);
+            //_rb.MovePosition(_rb.position + _speed);
+            _rb.MoveRotation(Quaternion.Euler(euler));
         }
 	}
     
@@ -51,6 +53,7 @@ public class SpaceshipControlBehaviour : MonoBehaviour {
     private System.Single _speed = 0.0F;
     private Vector3 _dir = new Vector3();
 
+    public System.Single _angularReaction = 10.0F;
     public System.Single _reaction = 1.0F;
     public System.Single _slowing = 1.5F;
     public System.Single _acceleration = 1.0F;
