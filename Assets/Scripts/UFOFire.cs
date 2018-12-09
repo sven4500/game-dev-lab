5c3c77530c4a893b46227bdf0ea9306be4ad43f2
Ivars Rozhleys <noname@noname.com>
2018-12-09 17:29:29 +0300

    Добавлены обект для лазера и материал для лпзера. Добпавлены скрипты для выстрела лазером.

---
+++ Assets/Scripts/UFOFire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOFire: MonoBehaviour {
+
+	void Update() {
+		if((_time += Time.deltaTime) > _fireRate && Input.GetKeyDown(KeyCode.Space) == true) {
+            Vector3 pos = transform.position;
+            Instantiate(Resources.Load("LaserCharge"), pos, Quaternion.identity);
+        }
+	}
+
+    private System.Single _time = 0.0F;
+    public System.Single _fireRate = 1.0F;
+
+}
